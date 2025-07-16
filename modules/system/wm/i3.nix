f302608c9559318f56ec006ebc7b3a62131e45f3
Janpstrunn <janpstrunn@disroot.org>
2025-07-16 19:16:50 -0300

    feat(system): Add i3 (WIP)

---
+++ modules/system/wm/i3.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  pkgs,
+  callPackage,
+  ...
+}: {
+  environment.pathsToLink = ["/libexec"];
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+    };
+    displayManager = {
+      defaultSession = "none+i3";
+    };
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+        i3blocks
+      ];
+    };
+  };
+  programs.i3lock.enable = true;
+}
